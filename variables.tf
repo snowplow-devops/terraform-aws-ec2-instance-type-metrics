1aca6d8795827d0ea00f8a7d19140922b4a78847
jbeemster <joshua.a.beemster@gmail.com>
2022-11-23 12:53:42 +1100

    Tune defaults to provide better split on memory allocation (closes #2)

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "instance_type" {
 }
 
 variable "memory_for_app_pct" {
-  description = "The percent of the instance memory to allocate to the application (default: 80)"
+  description = "The percent of the instance memory to allocate to the application"
   type        = number
   default     = 80
 }
@@ -12,5 +12,5 @@ variable "memory_for_app_pct" {
 variable "memory_for_os_mb" {
   description = "The minimum amount of memory that must be available for the base Operation System"
   type        = number
-  default     = 256
+  default     = 384
 }
